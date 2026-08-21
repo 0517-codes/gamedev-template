02475f72cf59c7ae9dcec460e29918384ae1496a
山崎恭太郎 <f2025135@kawasaki.kst-h.ed.jp>
2026-08-21 06:09:48 +0000

    bisyojo

--- src/scenes/ResultScene.ts
+++ src/scenes/ResultScene.ts
@@ -13,6 +13,10 @@ export class ResultScene extends Phaser.Scene {
     super('ResultScene');
   }
 
+  preload(): void {
+    this.load.image('simapanbisyojo', 'simapanbisyojo.webp');
+  }
+
   create(data: ResultData): void {
     const { width, height } = this.scale;
     const won = data.won;
@@ -20,6 +24,10 @@ export class ResultScene extends Phaser.Scene {
       const color = '#f1f1f1';
 
       this.add.rectangle(width / 2, height / 2, width, height, 0x222222);
+    if (won) {
+      const image = this.add.image(width - 220, height / 2, 'simapanbisyojo');
+      image.setScale(420 / image.height);
+    }
     this.add.text(width / 2, 190, title, {
       fontFamily: 'Georgia, serif',
       fontSize: '64px',
